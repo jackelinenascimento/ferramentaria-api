e56538daef2839a1560f9a579ca97fd059361088
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2021-12-10 16:59:43 -0300

    config seguranca e postgres

--- src/main/java/br/com/ferramentaria/api/configs/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/ferramentaria/api/configs/security/AutenticacaoViaTokenFilter.java
@@ -7,17 +7,45 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.ferramentaria.api.entity.Usuario;
+import br.com.ferramentaria.api.repository.UsuarioRepository;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 
+	private TokenService tokenService;
+	
+	private UsuarioRepository usuarioRepository;
+	
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
+	}
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
 		String token = recuperarToken(request);
+		boolean valido = tokenService.isTokenValido(token);
+		if(valido) {
+			autenticarCliente(token);
+		}
 		
 		filterChain.doFilter(request, response);		
+	}
+
+	private void autenticarCliente(String token) {
+		
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = usuarioRepository.getById(idUsuario);
+		
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
 	}
 
 	private String recuperarToken(HttpServletRequest request) {
--- src/main/java/br/com/ferramentaria/api/configs/security/SecurityConfigurations.java
+++ src/main/java/br/com/ferramentaria/api/configs/security/SecurityConfigurations.java
@@ -15,12 +15,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import br.com.ferramentaria.api.repository.UsuarioRepository;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@Override
 	@Bean
@@ -51,7 +59,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and().csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+			.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 		
 		http.headers().frameOptions().disable();
 		
